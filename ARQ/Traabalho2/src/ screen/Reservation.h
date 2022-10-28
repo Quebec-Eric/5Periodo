b0abe171c0b756973ffd487076a084d6877fde60
quebec-Eric <quebec.ericjs@gmail.com>
2022-10-28 18:27:32 -0300

    Inicio_TB2

---
+++ ARQ/Traabalho2/src/ screen/Reservation.h
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+class Reservation
+{
+private:
+    int chegada=nullptr;
+    string Busy =nullptr;
+    string intruction_String=nullptr;
+    string intruction_
+    
+public:
+    Reservation(/* args */);
+    ~Reservation();
+};
+
+Reservation::Reservation(/* args */)
+{
+}
+
+Reservation::~Reservation()
+{
+}
---
+++ ARQ/Traabalho2/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "screen/Reservation.h"
+
+
+int main()
+{
+    
+}
+
+
+
