9ed4ca6e11fb3c38245eea9b6e5d5ee7dad378bb
Quebec-Eric <quebec.ericjs@gmail.com>
2022-12-01 15:23:48 -0300

    PAA

--- PAA/Trabalho1/Pontos_Eric.c
+++ PAA/Trabalho1/Pontos_Eric.c
@@ -2,17 +2,22 @@
 #include <float.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h>
 
-
-int indice=0;
+double distancia_global=0;
 typedef struct 
 {
     int indice;
-    int eixo_x;
-    int eixo_Y;
+    double eixo_x;
+    double eixo_Y;
 }Pontos_Eric;
 
-void Mostrar_Rf(){
+typedef struct{
+    Pontos_Eric resposta;
+
+}Resposta;
+
+void mostrar_Rf(){
     printf("-----------------------\n");
     printf("Programa feito por Eric\n");
     printf("-----------------------\n\n\n");
@@ -21,138 +26,230 @@ void Mostrar_Rf(){
     
 }
 
-Pontos_Eric Criar_Dados(int quantidade , int indice){
-
+Pontos_Eric gerar_Dados(int quantidade , int indice){
     Pontos_Eric pontos; 
-    pontos.eixo_x =rand()%quantidade;
-    pontos.eixo_Y=rand()%quantidade;
+    pontos.eixo_x =((double)rand()/(double)(RAND_MAX)*quantidade);
+    pontos.eixo_Y=((double)rand()/(double)(RAND_MAX)*quantidade);
     pontos.indice=indice;
     return pontos;
 }
 
-int EntradaDado(){
+int entrada_Dados(){
     int valor=0;
     scanf("%d",&valor);
     return valor;
 }
 
-float Distancia_Euclidiana(Pontos_Eric PontosP1,Pontos_Eric Pontosp2){
+void toString(Resposta r[], double distancia){
 
-    return sqrt( (PontosP1.eixo_x- Pontosp2.eixo_x)*
-                (PontosP1.eixo_x- Pontosp2.eixo_x)+
-                (PontosP1.eixo_Y- Pontosp2.eixo_Y)*
-                (PontosP1.eixo_Y- Pontosp2.eixo_Y));
+    printf("\nPrimeiro ponto esta [%lf] do eixo X e [%lf] do eixo Y\n", r[0].resposta.eixo_x , r[0].resposta.eixo_Y);
+    printf("Segundo ponto esta [%lf] do eixo X e [%lf] do eixo Y\n", r[1].resposta.eixo_x , r[1].resposta.eixo_Y);
+    printf("Com a distanncia de [%lf] \n", distancia);
+    printf("-----------------------------------------------\n");
+
 }
 
-void Comparar_Todos(Pontos_Eric *PontosP, int quantidade){
+double calcular_Distancia(Pontos_Eric a , Pontos_Eric b){
+    double distancia =-1;
 
-    int Ponto1=0;
-    int Ponto2=0;
-    float distanncia=FLT_MAX;
-    for (int i = 0; i < quantidade; i++)
+    double distancia_X= a.eixo_x -b.eixo_x;
+    double distancia_Y=a.eixo_Y - b.eixo_Y;
+    distancia= sqrt(pow(distancia_X,2)+pow(distancia_Y,2));
+    return distancia;
+}
+
+void comparar_Todos(Pontos_Eric Pontos[] , int quantidade_pontos){
+    Resposta resp[2];
+    double menor_distancia=DBL_MAX;
+
+    for (int  i = 0; i < quantidade_pontos; i++)
     {
-        for (int z=i+1; z <quantidade; z++)
+        for (int z = i+1; z < quantidade_pontos; z++)
         {
-            if (Distancia_Euclidiana(PontosP[i],PontosP[z])< distanncia)
+            double ds = calcular_Distancia(Pontos[i],Pontos[z]);
+            if (ds<menor_distancia)
             {
-                distanncia=Distancia_Euclidiana(PontosP[i],PontosP[z]);
-                Ponto1=PontosP[i].indice;
-                Ponto2=PontosP[z].indice;
+                distancia_global= ds;
+                menor_distancia=ds;
+                resp[0].resposta=Pontos[i];
+                resp[1].resposta=Pontos[z];
+
             }
-            
+                
+        }
+        
+    }   
+    printf("Utilizando algoritmo de 0(n²)");
+    toString(resp,distancia_global);
+    
+    return;
+}
+
+int esq(int n){
+    return ((n+1)*2)-1;
+}
+int dir(int n){
+    return (n+1)*2;
+}
+
+void construcao_heap(Pontos_Eric P[], int i , int quantidade, bool x){
+
+    int esqueda=esq(i);
+    int direira=dir(i);
+    int maior=-1;
+
+    if(x){
+        if(esqueda<quantidade&&P[esqueda].eixo_x>P[i].eixo_x){ 
+        maior=esqueda;
+        }
+        else{
+            maior=i;
+        }
+
+        if(direira<quantidade&&P[direira].eixo_x>P[i].eixo_x){
+            maior=direira;
+        }
+    }
+    else{
+        if(esqueda<quantidade&&P[esqueda].eixo_Y>P[i].eixo_Y){ 
+        maior=esqueda;
+        }
+        else{
+            maior=i;
+        }
+
+        if(direira<quantidade&&P[direira].eixo_Y>P[i].eixo_Y){
+            maior=direira;
+        }        
+    }
+
+
+    if(maior!=i){
+
+        Pontos_Eric m=P[i];
+        P[i]=P[maior];
+        P[i]=m;
+
+        construcao_heap(P,maior,quantidade,x);
+    }
+
+}
+
+
+
+Pontos_Eric * divisao_E_conquista(Pontos_Eric x[], Pontos_Eric y[],int esquerda , int direita){
+    Pontos_Eric *res=(Pontos_Eric*)malloc(sizeof(Pontos_Eric)*2);
+
+    double delt=0;
+    int meio=0;
+    int contador=0;
+    
+    meio=esquerda+(direita-esquerda)/2;
+   Pontos_Eric *esquerda_P=divisao_E_conquista(x,y,esquerda,meio);
+   Pontos_Eric *direita_P=divisao_E_conquista(x,y,meio+1,direita);
+
+
+    if(calcular_Distancia(esquerda_P[0],esquerda_P[1])<calcular_Distancia(direita_P[0],direita_P[1])){
+        res=esquerda_P;
+    }else{
+         res=direita_P;
+    }
+
+    delt = calcular_Distancia(res[0], res[1]);
+
+    Pontos_Eric *linhaD= (Pontos_Eric*)malloc(sizeof(Pontos_Eric)*sizeof(y));
+
+
+    for (int i = 0; i < sizeof(y); i++)
+    {
+        if(delt>abs(x[meio].eixo_x-y[i].eixo_x)){
+            linhaD[contador]=y[i];
+            contador++;
+        }
+    }
+    
+    for(int i=0;i<contador;i++){
+        int j=i+1;
+        while (j<contador&& delt>calcular_Distancia(linhaD[i],linhaD[j]))
+        {
+            delt=calcular_Distancia(linhaD[i],linhaD[j]);
+            res[0]=linhaD[i];
+            res[1]=linhaD[j];
         }
         
     }
-    
-    printf("Os pontos mais proximos sao %d   %d \n",Ponto1,Ponto2);
-    printf("Com a distancia de %lf", distanncia);
-    
 
-
-
+    return res;
 }
 
 
-void Intercalar(Pontos_Eric*Pontos, int esquerda, int meio, int direita){
-
-    int n1=0;
-    int n2=0;
-    int i=0;
-    int j=0;
-    int k =0;
-
-    n1=meio-esquerda+1;
-    n2=direita-meio;
-    
-    Pontos_Eric e1[n1+1];
-    Pontos_Eric e2[n2+1];
 
 
-    for (i = 0; i < n1; i++)
+Pontos_Eric * heapShort(Pontos_Eric P[], int quantidade ,bool x ){
+    Pontos_Eric *pontos =(Pontos_Eric*)malloc(sizeof(Pontos_Eric)*quantidade);
+
+    for (int i = 0; i < quantidade; i++)
     {
-        e1[i]=Pontos[esquerda+i];
-    }
-
-
-    for (j = 0; j < n2; j++)
-    {
-        e2[i]=Pontos[meio+j+1];
+        pontos[i]=P[i];
     }
     
-
-    
-
-
-}
-
-
-void Mergsort(Pontos_Eric *Pontos, int esquerda, int direita){
-
-    if(esquerda<direita){
-        int meio = (Pontos,esquerda+direita)/2;
-        Mergsort(Pontos,esquerda,meio);
-        Mergsort(Pontos,meio+1, direita);
-        Intercalar(Pontos,esquerda,meio,direita);
-    }
-}
-
-
-void DivisaoEConquita(Pontos_Eric *PontosP, int n){
-
-
-
-
-}
-
-
-
-void Achar_Pontos_Proximos(Pontos_Eric *PontosP, int n){
-
-    int fazer=0;
-    printf("\nGotsaria de fazer 1=O(n²) ou  2=O(nlogn)\n");
-    fazer=EntradaDado();
-    if (fazer==1)
+    for (int i = quantidade/2; i >=0; i--)
     {
-        Comparar_Todos(PontosP,n);
+        construcao_heap(pontos,i,quantidade,x);
     }
-    else{
-        DivisaoEConquista(PontosP,n);
+    
+    for(int i= quantidade-1;i>0;i--){
+
+        Pontos_Eric b = pontos[0];
+        pontos[0]=pontos[i];
+        pontos[i]=b;
+
+        construcao_heap(pontos,0,i,x);
     }
+    
+    return pontos;
+}
+
+
+
+
+void  iniciando_Divisao_E_Conquista(Pontos_Eric Pontos[], int quantidade){
+
+    //Resposta resp[2];
+    Pontos_Eric *e= heapShort(Pontos,quantidade,true);
+    Pontos_Eric *y= heapShort(Pontos,quantidade,false);    
+    
+    divisao_E_conquista(e,y,0,sizeof(e)-1);
+    
+    /*for (int  i = 0; i < quantidade; i++)
+    {
+     printf("%lf %lf %i\n", e[i].eixo_x,e[i].eixo_Y,e[i].indice);
+    
+    } 
+    */   
+    
     
 
     return;
 }
 
+void iniciar_Problema(Pontos_Eric Pontos[], int quantidade_pontos){
+    comparar_Todos(Pontos,quantidade_pontos);
+    iniciando_Divisao_E_Conquista(Pontos, quantidade_pontos);
+    return;
+} 
+
 
 
 int main(int argc, char **argv){
-    Mostrar_Rf();
-    int valor=EntradaDado();    
-    Pontos_Eric *PontosP = (Pontos_Eric*)malloc(valor*sizeof(Pontos_Eric));
-    for (int i = 0; i <valor; i++)
+    mostrar_Rf();
+    int quantidade = entrada_Dados();
+    Pontos_Eric pontos_no_espaco[quantidade];
+    for (int i = 0; i < quantidade; i++)
     {
-        PontosP[i]=Criar_Dados(valor,i);
+        pontos_no_espaco[i]=gerar_Dados(quantidade,i);
+       // printf("%lf %lf %i\n", pontos_no_espaco[i].eixo_x,pontos_no_espaco[i].eixo_Y , pontos_no_espaco[i].indice);
     }
-    Achar_Pontos_Proximos(PontosP,valor);
+    iniciar_Problema(pontos_no_espaco, quantidade);   
     return 0;
 }
